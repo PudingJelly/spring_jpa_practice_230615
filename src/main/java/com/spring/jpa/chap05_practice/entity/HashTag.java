94088fcd5a636af1725bfc991b10153ef5c2845c
changju <changju7857@gmail.com>
2023-06-19 13:23:39 +0900

    # 6. 종합 연습예제 1

---
+++ src/main/java/com/spring/jpa/chap05_practice/entity/HashTag.java
@@ -0,0 +1,29 @@
+package com.spring.jpa.chap05_practice.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Setter
+@Getter
+@ToString(exclude = {"post"})
+@EqualsAndHashCode(of = {"id"})
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "tbl_hash_tag")
+public class HashTag {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "tag_no")
+    private long id;
+
+    private String tagName; // 해쉬태그 이름
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_no") // 실제 컬럼명으로 작성
+    private Post post;
+
+}
---
+++ src/main/java/com/spring/jpa/chap05_practice/entity/Post.java
@@ -0,0 +1,44 @@
+package com.spring.jpa.chap05_practice.entity;
+
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Setter
+@Getter
+@ToString(exclude = {"hashTags"})
+@EqualsAndHashCode(of = {"id"})
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "tbl_post")
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "post_no")
+    private long id;
+
+    @Column(nullable = false)
+    private String writer; // 작성자
+
+    private String content; // 내용
+
+    @CreationTimestamp
+    @Column(updatable = false) // 한번 인서트 이후엔 다시는 변하지 않는다
+    private LocalDateTime createDate; // 작성시간
+
+    @UpdateTimestamp
+    private LocalDateTime updateDate; // 수정시간
+
+    // 양방향 연관관계를 위한 초기화 설정
+    @OneToMany(mappedBy = "post") // 선언된 변수명으로 작성
+    private List<HashTag> hashTags = new ArrayList<>();
+
+}
